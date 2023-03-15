1cae78e33ee34abbc6cc8a6bf63fd6fed4b58147
DuGuYifei <liuyifei97@163.com>
2023-03-15 01:39:23 +0100

    Init main window and create projects browser dialog

---
+++ GenesisEditor/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using GenesisEditor.GameProject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GenesisEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += OnMainWindowLoaded;
+        }
+
+        private void OnMainWindowLoaded(object sender, RoutedEventArgs e)
+        {
+            Loaded -= OnMainWindowLoaded;
+            OpenProjectBrowserDialog();
+        }
+
+        private void OpenProjectBrowserDialog()
+        {
+            ProjectBrowserDialg projectBrowser = new ProjectBrowserDialg();
+            if (projectBrowser.ShowDialog() == false)
+                Application.Current.Shutdown();
+            else
+            {
+
+            }
+        }
+    }
+}
